02376f2341e978528963b32b9dbd13eabf695b6f
MdEmon1346 <emamulhoque1346@gmail.com>
2026-03-03 23:59:14 +0600

    second commit

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -1,17 +1,19 @@
 import { use } from "react";
 import Player from "../Players.jsx/Player";
 
-const AvailablePlayers = ({playersPromise}) => {
-   const players = use(playersPromise);
-//    console.log(players);
-    return (
-        <div className="max-w-7xl mx-auto" >
-            <h2>Available Players: {players.length}</h2>
-            {
-                players.map(player=><Player key={player.id} player={player}></Player>)
-            }
-        </div>
-    );
+const AvailablePlayers = ({ playersPromise }) => {
+  const players = use(playersPromise);
+  //    console.log(players);
+  return (
+    <div className="max-w-7xl mx-auto">
+      <h2>Available Players: {players.length}</h2>
+      <div className="max-w-7xl mx-auto grid grid-cols-3">
+        {players.map((player) => (
+          <Player key={player.id} player={player}></Player>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default AvailablePlayers;
--- src/components/Players.jsx/Player.jsx
+++ src/components/Players.jsx/Player.jsx
@@ -3,7 +3,15 @@ import flgImg from "../../assets/flag.png";
 
 const Player = ({ player }) => {
   console.log(player);
-  const { playerImg, playerName } = player;
+  const {
+    playerImg,
+    playerName,
+    playerCountry,
+    rating,
+    battingStyle,
+    bowlingStyle,
+    price,
+  } = player;
   return (
     <div className="card w-96 shadow-sm  py-4 my-4 ">
       <figure>
@@ -14,18 +22,37 @@ const Player = ({ player }) => {
           <img className="w-4 h-4" src={nameImg} alt="" />
           <h3 className="card-title">Player Name: {playerName} </h3>
         </div>
-        <div className="flex gap-2 items-center border-b pb-2 ">
-          <img className="w-3 h-3 opacity-50 " src={flgImg} alt="flag" />
-          <h3 className="card-title font-thin text-base">
-            Player Name: {playerName}
-          </h3>
-          <button className=" bg-gray-200 border-none w-30 h-9 rounded-md">
-            Default
+
+        <div className="justify-between flex items-center pb-2 border-b border-gray-300">
+          <div className="flex gap-2 items-center  ">
+            <img className="w-3 h-3 opacity-50 " src={flgImg} alt="flag" />
+            <h2 className="card-title font-thin text-base">{playerCountry}</h2>
+          </div>
+
+          <button className=" bg-gray-200 border-none w-25 h-6 rounded-sm font-thin text-base">
+            All Rounder
           </button>
         </div>
 
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+        <div className="justify-between flex items-center font-bold">
+          <span>Ratings: </span>
+          <span>{rating}</span>
+        </div>
+
+        <div className="justify-between flex items-center ">
+          <span className="font-bold">{battingStyle} </span>
+          <span>{bowlingStyle}</span>
+        </div>
+
+        <div className="flex justify-between items-center">
+          <div className="flex ">
+            <h2 className="font-bold">Price: {price}</h2>
+            <span className="pl-1 font-bold">USD</span>
+          </div>
+
+          <button className=" bg-gray-200 border-none w-30 h-8 rounded-sm font-thin text-sm">
+            Choose Player
+          </button>
         </div>
       </div>
     </div>
